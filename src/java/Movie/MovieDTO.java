a3293f51c0ce3e127666d40918a8385c2433f76d
Mingo20Percent <163963088+trungnam1644@users.noreply.github.com>
2025-03-15 17:40:31 +0700

    .

--- src/java/Movie/MovieDTO.java
+++ src/java/Movie/MovieDTO.java
@@ -5,10 +5,139 @@
  */
 package Movie;
 
+import java.util.List;
+
 /**
  *
  * @author trung
  */
 public class MovieDTO {
-    
+    private int movieID;
+    private String title;
+    private String description;
+    private int releaseYear;
+    private String countryName;
+    private double rating;
+    private String videoURL;
+    private String trailerURL;
+    private String thumbnailURL;
+    private String userName;
+    private List<String> genres;
+    private List<String> actors;
+
+    public MovieDTO(int movieID, String title, String description, int releaseYear, String countryName, double rating,
+                    String videoURL, String trailerURL, String thumbnailURL, String userName,
+                    List<String> genres, List<String> actors) {
+        this.movieID = movieID;
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.countryName = countryName;
+        this.rating = rating;
+        this.videoURL = videoURL;
+        this.trailerURL = trailerURL;
+        this.thumbnailURL = thumbnailURL;
+        this.userName = userName;
+        this.genres = genres;
+        this.actors = actors;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getTrailerURL() {
+        return trailerURL;
+    }
+
+    public void setTrailerURL(String trailerURL) {
+        this.trailerURL = trailerURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<String> actors) {
+        this.actors = actors;
+    }
+
+   
+
 }
