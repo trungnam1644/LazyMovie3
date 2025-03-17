2902618c12e6df9b15f67ddf50e7a095cc628422
Mingo20Percent <163963088+trungnam1644@users.noreply.github.com>
2025-03-17 20:49:54 +0700

    .

--- src/java/controller/SearchController.java
+++ src/java/controller/SearchController.java
@@ -51,7 +51,7 @@ public class SearchController extends HttpServlet {
         }
     }
     }
-
+//hellooooooooooooooooooo
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
